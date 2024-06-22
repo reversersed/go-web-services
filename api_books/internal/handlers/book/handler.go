b12ae51af055e32de7144c70c83e469254f5491a
reversersed <myregisg@gmail.com>
2024-06-22 08:59:03 +0300

    Made book handler Need to add file saving

--- api_books/internal/handlers/book/handler.go
+++ api_books/internal/handlers/book/handler.go
@@ -2,7 +2,12 @@ package book
 
 import (
 	"context"
+	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/reversersed/go-web-services/tree/main/api_books/internal/client"
@@ -31,7 +36,7 @@ func (h *Handler) Register(route *httprouter.Router) {
 
 func (h *Handler) AddBookHandler(w http.ResponseWriter, r *http.Request) error {
 	r.ParseMultipartForm(10 << 20) //10 Mb
-	file, handler, err := r.FormFile("file")
+	file, header, err := r.FormFile("file")
 	if err != nil {
 		return errormiddleware.BadRequestError([]string{"you need to upload file"}, err.Error())
 	}
@@ -40,9 +45,49 @@ func (h *Handler) AddBookHandler(w http.ResponseWriter, r *http.Request) error {
 	if err != nil {
 		return errormiddleware.BadRequestError([]string{"invalid author id"}, err.Error())
 	}
+	genres := make([]primitive.ObjectID, len(strings.Split(r.FormValue("genres"), ",")))
+	wg := sync.WaitGroup{}
+	errchan := make(chan error, 1)
 
+	for idx, val := range strings.Split(r.FormValue("genres"), ",") {
+		wg.Add(1)
+		go func(idx int, value string, wg *sync.WaitGroup) {
+			genres[idx], err = primitive.ObjectIDFromHex(value)
+			if err != nil {
+				errchan <- err
+			}
+			wg.Done()
+		}(idx, val, &wg)
+	}
+	wg.Wait()
+	close(errchan)
+	select {
+	case err = <-errchan:
+		return errormiddleware.BadRequestError([]string{"invalid genre id"}, err.Error())
+	default:
+		break
+	}
+	year, err := strconv.Atoi(r.FormValue("year"))
+	if err != nil {
+		return errormiddleware.BadRequestError([]string{"year must be a number"}, err.Error())
+	}
 	query := &client.InsertBookQuery{
 		Name:     r.FormValue("name"),
 		AuthorId: author,
+		GenresId: genres,
+		Year:     year,
+		File:     file,
 	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	book, err := h.BookService.AddBook(ctx, query)
+	if err != nil {
+		return err
+	}
+	//save file and write path
+
+	w.WriteHeader(http.StatusCreated)
+	bytes, _ := json.Marshal(&book)
+	w.Write(bytes)
 }
